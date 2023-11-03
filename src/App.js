cb3ade83f6ffeb048a6b684184a6655fcdae332b
Vikash Kumar <vikasthakur9310@gmail.com>
2023-11-03 23:03:43 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,57 +1,13 @@
-import React, { useEffect, useState } from 'react'
-import Loading from './Loading';
-import Tours from './Tours';
+import React from 'react'
 
-const url =  'https://course-api.com/react-tours-project'
+
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [tours, setTours] = useState([]);
+ 
+    
 
-  const removeTour = (id) => {
-    const newTours = tours.filter((tour) => tour.id !== id)
-    setTours(newTours)
-  }
-
-  const fetchTours = async () => {
-    setLoading(true);
-    try {
-      const reponse = await fetch(url)
-      const tours = await reponse.json()
-      setLoading(false)
-      setTours(tours);
-    } catch (error){
-      setLoading(false)
-      console.log(error)
-    }
-  }
-
-  useEffect(() => {
-    fetchTours()
-  }, [])
-  
-  if (loading) {
-    return (
-      <main>
-        <Loading/>
-      </main>
-    )
-  }
-
-  if (tours.lenth === 0) {
-    return (
-      <main>
-        <div className='title'>
-          <h2>no tours left</h2>
-          <button className='btn'>refresh</button>
-        </div>
-      </main>
-    )
-  }
   
   return (
-    <main>
-      <Tours tours={tours} removeTour={removeTour} />
-    </main>
+   
   )
 }
 
