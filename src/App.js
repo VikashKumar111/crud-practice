f0c9ef09bb0610a7edadf1576b0b849e9b5940ce
Vikash Kumar <vikasthakur9310@gmail.com>
2023-11-03 12:39:51 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+import React from 'react'
 function App() {
   return <h2>Tours Project Setup</h2>
 }
