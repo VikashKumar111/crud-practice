abffefbcaf4fd3195e35a6a92ab757b78506e49e
Vikash Kumar <vikasthakur9310@gmail.com>
2023-11-03 22:27:50 +0530

    add

--- src/Tour.js
+++ src/Tour.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Tour = ({id,image,info,name,price,removeTour}) => {
+const Tour = ({ id, image, info, name, price, removeTour }) => {
+    const [readmore, setReadmore] = useState(false);
     return (
         <article className="single-tour">
             <img src={image} alt={name} />
@@ -9,8 +10,8 @@ const Tour = ({id,image,info,name,price,removeTour}) => {
                     <h4>{name}</h4>
                     <h4 className="tour-price">${price}</h4>
                 </div>
-                <p> {`${info.substring(0,200)}...`}
-                    <button>read more</button>
+                <p> {readmore ? info :`${info.substring(0,200)}...`}
+                    <button onClick={()=>setReadmore(!readmore)}>{readmore ? 'show less' : 'read more'}</button>
                 </p>
                 <button className="delete-btn" onClick={()=> removeTour(id)}>
                     not interested
