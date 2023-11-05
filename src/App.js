e88dc86ebd0ff98934e0fafd7c241d123f8802da
Vikash Kumar <vikasthakur9310@gmail.com>
2023-11-05 22:22:49 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React,{useState} from 'react'
+import Review from './Review'
 
 
 function App() {
--- src/Review.js
+++ src/Review.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {FaChevronLeft,FaChevronRight,FaQuoteRight} from 'react-icons/fa';
 import people from './data';
 
 const Review = () => {
+    const [index, setIndex] = useState(0);
+    const { name, job, image, text } = people[index];
     return (
-        <article>
+        <article className='review'>
+            <div className='img-container'>
+                <img src={image} alt={name} className='person-img' />
+                <span className='quote-icon'><FaQuoteRight/></span>
+            </div>
+            <h4 className='author'>{name}</h4>
+            <p className='job'>{job}</p>
+            <p className='info'>{text}</p>
+            <div>
+                <button className='prev-btn'><FaChevronLeft/></button>
+                <button className='next-btn'><FaChevronRight/></button>
+            </div>
+            <button>surprise me</button>
 
         </article>
     )
