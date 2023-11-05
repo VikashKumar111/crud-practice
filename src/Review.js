d57a0bced98f6b5a72788824d5e22429c96b0f28
Vikash Kumar <vikasthakur9310@gmail.com>
2023-11-05 22:47:45 +0530

    add

--- src/Review.js
+++ src/Review.js
@@ -5,6 +5,36 @@ import people from './data';
 const Review = () => {
     const [index, setIndex] = useState(0);
     const { name, job, image, text } = people[index];
+
+    const checkNumber = (number) => {
+        if (number > people.length - 1) {
+            return 0;
+        }
+        if (number < people.length - 1) {
+            return people.length - 1;
+        }
+        return number;
+    };
+
+    const nextPerson = () => {
+        setIndex((index) => {
+            let newIndex = index + 1;
+            return checkNumber(newIndex);
+        })
+    };
+    const prevPerson = () => {
+        setIndex((index) => {
+            let newIndex = index - 1;
+            return checkNumber(newIndex);
+        })
+    };
+    const randomPerson = () => {
+        let randomNumber = Math.floor(Math.random() * people.length);
+        if (randomNumber === index) {
+            randomNumber = index + 1;
+        }
+        setIndex(checkNumber(randomNumber));
+    }
     return (
         <article className='review'>
             <div className='img-container'>
@@ -15,10 +45,10 @@ const Review = () => {
             <p className='job'>{job}</p>
             <p className='info'>{text}</p>
             <div>
-                <button className='prev-btn'><FaChevronLeft/></button>
-                <button className='next-btn'><FaChevronRight/></button>
+                <button className='prev-btn' onClick={prevPerson()}><FaChevronLeft/></button>
+                <button className='next-btn'onClick={nextPerson()}><FaChevronRight/></button>
             </div>
-            <button>surprise me</button>
+            <button className='random-btn' onClick={randomPerson()}>surprise me</button>
 
         </article>
     )
